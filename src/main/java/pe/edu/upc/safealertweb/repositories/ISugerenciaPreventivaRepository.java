f3d3c35fb702e4fa37ba2ad57c88b3f337c9cb7b
Diegou2203 <129676670+Diegou2203@users.noreply.github.com>
2025-04-20 21:00:33 -0500

    Delete src/main/java/pe/edu/upc/safealertweb/repositories/ISugerenciaPreventivaRepository.java

--- src/main/java/pe/edu/upc/safealertweb/repositories/ISugerenciaPreventivaRepository.java
+++
@@ -1,7 +0,0 @@
-package pe.edu.upc.safealertweb.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pe.edu.upc.safealertweb.entities.SugerenciaPreventiva;
-
-public interface ISugerenciaPreventivaRepository  extends JpaRepository<SugerenciaPreventiva, Integer> {
-}

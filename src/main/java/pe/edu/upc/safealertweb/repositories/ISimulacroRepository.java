650c20130387e94bf95fa06766603b29d33270c1
eloydadaea <57968579+eloydadaea@users.noreply.github.com>
2025-04-23 09:42:03 -0500

    Update ISimulacroRepository.java

--- src/main/java/pe/edu/upc/safealertweb/repositories/ISimulacroRepository.java
+++ src/main/java/pe/edu/upc/safealertweb/repositories/ISimulacroRepository.java
@@ -3,5 +3,6 @@ package pe.edu.upc.safealertweb.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pe.edu.upc.safealertweb.entities.Simulacro;
 
+@Repository
 public interface ISimulacroRepository extends JpaRepository<Simulacro, Integer> {
 }

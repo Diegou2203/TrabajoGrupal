aadb395b7fc02381d4c50b15ec81beea8d05a158
ManuelOrellanaReyes <u202214538@upc.edu.pe>
2025-04-23 22:00:34 -0500

    Agregar Query de Respuesta

--- src/main/java/pe/edu/upc/safealertweb/repositories/IRespuestaRepository.java
+++ src/main/java/pe/edu/upc/safealertweb/repositories/IRespuestaRepository.java
@@ -1,9 +1,16 @@
 package pe.edu.upc.safealertweb.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pe.edu.upc.safealertweb.entities.Respuesta;
 
+import java.util.List;
+
 @Repository
 public interface IRespuestaRepository extends JpaRepository<Respuesta, Integer> {
+    @Query(value = "SELECT cc.contenido, cc.estado, COUNT(id_respuesta) \n" +
+            "FROM comentario_consulta cc INNER JOIN respuesta r ON cc.id_comentario=r.id_comentario \n" +
+            "WHERE r.fechacreacion = CURRENT_DATE GROUP BY cc.contenido, cc.estado", nativeQuery = true)
+    public List<String[]> cantidadRespuestasPorComentario();
 }
--- src/main/java/pe/edu/upc/safealertweb/servicesinterfaces/IRespuestaService.java
+++ src/main/java/pe/edu/upc/safealertweb/servicesinterfaces/IRespuestaService.java
@@ -1,14 +1,19 @@
 package pe.edu.upc.safealertweb.servicesinterfaces;
 
-import pe.edu.upc.safealertweb.entities.ComentarioConsulta;
 import pe.edu.upc.safealertweb.entities.Respuesta;
 
 import java.util.List;
 
 public interface IRespuestaService {
     public List<Respuesta> list();
+
     public void insert(Respuesta re);
+
     public Respuesta listarId(int idRespuesta);
+
     public void delete(int idRespuesta);
+
     public void update(Respuesta re);
+
+    public List<String[]> cantidadRespuestasPorComentario();
 }
